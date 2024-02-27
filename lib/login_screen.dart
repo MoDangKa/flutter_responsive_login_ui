cfde22f148366574b2bd2b0f12ae7cc538746d8d
Thanaphon <thanaphon.p@i3gateway.com>
2024-02-27 17:16:46 +0700

    perf: update code

---
+++ lib/login_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/widgets/social_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/images/signin_balls.png'),
+              const Text(
+                "Sign in.",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                ),
+              ),
+              const SizedBox(height: 50),
+              const SocialButton(
+                  iconPath: "assets/svgs/g_logo.svg",
+                  label: "continue with Google"),
+              const SizedBox(height: 20),
+              const SocialButton(
+                iconPath: "assets/svgs/f_logo.svg",
+                label: "continue with Facebook",
+                horizontalPadding: 90,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
