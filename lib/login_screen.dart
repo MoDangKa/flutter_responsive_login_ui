95524b0c43db41ef6a75f2ae9f05867cf0f87119
Thanaphon <thanaphon.p@i3gateway.com>
2024-02-27 17:53:25 +0700

    perf: update code

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_responsive_login_ui/widgets/gradient_button.dart';
+import 'package:flutter_responsive_login_ui/widgets/login_field.dart';
 import 'package:flutter_responsive_login_ui/widgets/social_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -21,14 +23,34 @@ class LoginScreen extends StatelessWidget {
               ),
               const SizedBox(height: 50),
               const SocialButton(
-                  iconPath: "assets/svgs/g_logo.svg",
-                  label: "continue with Google"),
+                iconPath: "assets/svgs/g_logo.svg",
+                label: "continue with Google",
+                // onPressed: () {},
+              ),
               const SizedBox(height: 20),
               const SocialButton(
                 iconPath: "assets/svgs/f_logo.svg",
                 label: "continue with Facebook",
                 horizontalPadding: 90,
+                // onPressed: () {},
               ),
+              const SizedBox(height: 15),
+              const Text(
+                "or",
+                style: TextStyle(
+                  fontSize: 17,
+                ),
+              ),
+              const SizedBox(height: 15),
+              const LoginField(
+                hintText: 'Email',
+              ),
+              const SizedBox(height: 15),
+              const LoginField(
+                hintText: 'Password',
+              ),
+              const SizedBox(height: 20),
+              const GradientButton()
             ],
           ),
         ),
